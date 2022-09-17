af1524ece0fe1a7cb0623656fae0707297bd4e0f
Rodrigo López <37187963+RodrigoFLP@users.noreply.github.com>
2022-09-17 08:00:10 -0600

    fix pagination range

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -7,6 +7,7 @@ export const getPaginationRange = (movies: SearchResult) => {
   if (movies.total_pages === movies.page) {
     start = movies.total_results - movies.results.length + 1;
     end = movies.total_results;
+    return { start, end };
   }
   start = (movies.page - 1) * movies.results.length + 1;
   end = movies.results.length * movies.page;
